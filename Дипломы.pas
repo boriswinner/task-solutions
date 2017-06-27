f4c1ffd62e184a9e7a11c8cac04711d116f140a4
Boris Timofeenko <timbori@mail.ru>
2017-06-27 19:36:20 +1000

    Update Дипломы.pas

--- Дипломы.pas
+++ Дипломы.pas
@@ -1,41 +1,36 @@
-program project1;
- 
-{$mode objfpc}{$H+}
- 
+program new_diploma;
+{$mode objfpc}
 uses Math;
+
+const
+  size = 1000000000000000000;
 var
-  W,H,N,arr: qword;
-function CheckOk(a: qword): boolean;
+  W,H,N: qword;
+
+function Check(a: qword): boolean;
 begin
   Result := (a div W) * (a div H) >= N;
 end;
- 
-function BinSearch (position,l,r: qword): qword;
+
+function BinSearch (l,r: qword): qword;
+var
+  m: qword;
 begin
-  if (r-l<=2) and (CheckOk(position)=true) then exit(position);
-  if (not CheckOk(position)) then
+  while (l < r - 1) do
   begin
-    l := position;
-    position := l + ((r - l) div 2);
-    if (r-l<=1) then position := r;
-    Result := BinSearch(position,l,r);
-  end else
-  begin
-    r := position;
-    position := l + ((r - l) div 2);
-    if (r-l<=1) then position := l;
-    Result := BinSearch(position,l,r);
+    m := (l + r) div 2;
+    if Check(m) then r := m else l := m;
   end;
+  exit(r);
 end;
- 
+
 begin
   Assign(input,'diploma.in');
   Assign(output,'diploma.out');
   reset(input);
   read(W, H, N);
   close(input);
-  arr := 1000000000000000000;
   rewrite(output);
-  write(BinSearch(arr div 2,1,arr));
+  write(BinSearch(0,size));
   close(output);
 end.
